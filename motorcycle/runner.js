ff559a4cf04adfffa14253ffa67fe6c74aa4d460
robbie.knowles <robbie.knowles@ucc.on.ca>
2018-05-23 14:05:20 -0400

    updates

--- motorcycle/runner.js
+++ motorcycle/runner.js
@@ -1,18 +1,11 @@
-var drawModule = (function () {
-
-    var paint = function () {
-
-    }
-
-
-    var init = function () {
-        gameloop = setInterval(paint, 80);
-    }
-
-
-    return {
-        init: init
-    };
-
-
-}());
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(0, mycanvas.height-5, 50, 50);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function draw() {
+    drawPaddle()
+}
+draw();
